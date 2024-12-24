4227f60efe6a11190872c3af5fdeee65eee482f8
Johnathan Barbosa <jbarbosa0504@gmail.com>
2024-12-23 23:44:37 -0300

    feat: implementing error handling

--- j_auth_client/client.py
+++ j_auth_client/client.py
@@ -1,9 +1,26 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from typing import Any, Dict, Optional
 
-from requests import post, request
 from jwt import decode
+from requests import (
+    ConnectionError,
+    HTTPError,
+    RequestException,
+    Timeout,
+    post,
+    request,
+)
 from requests.auth import HTTPBasicAuth
+
+from j_auth_client.exceptions import (
+    JAuthAuthenticationException,
+    JAuthBaseException,
+    JAuthClientException,
+    JAuthConnectionException,
+    JAuthRequestException,
+    JAuthServerException,
+    JAuthTimeoutException,
+)
 
 
 class JAuthClient:
@@ -21,23 +38,36 @@ class JAuthClient:
         self.auth_url = auth_url
 
     def __authenticate(self):
-        response = self.request(
-            method=post,
-            url=self.auth_url,
-            data={"grant_type": "client_credentials"},
-            headers={"Content-Type": "application/x-www-form-urlencoded"},
-            auth=HTTPBasicAuth(self.username, self.password),
-            auth_server_authenticated=False,
-        )
+        try:
+            response = self.request(
+                method=post,
+                url=self.auth_url,
+                data={"grant_type": "client_credentials"},
+                headers={"Content-Type": "application/x-www-form-urlencoded"},
+                auth=HTTPBasicAuth(self.username, self.password),
+                auth_server_authenticated=False,
+            )
 
-        self.token = response["access_token"]
-        self.token_expires_at = datetime.fromtimestamp(
-            int(decode(jwt=self.token, options={"verify_signature": False})["exp"])
-        )
+            self.token = response["access_token"]
+            self.token_expires_at = datetime.fromtimestamp(
+                int(decode(jwt=self.token, options={"verify_signature": False})["exp"])
+            )
+
+        except (JAuthClientException, JAuthServerException) as error:
+            raise JAuthRequestException(
+                message=f"Error on authentication request: {error.status_code} - {error.message} ",
+            )
+
+        except JAuthBaseException as error:
+            raise JAuthAuthenticationException(
+                message=f"Error on authentication request: {error.message}",
+            )
 
     @property
     def __should_authenticate(self):
-        return not self.token or self.token_expires_at < datetime.now()
+        return not self.token or self.token_expires_at < (
+            datetime.now() - timedelta(minutes=1)
+        )
 
     def request(
         self,
@@ -57,7 +87,36 @@ class JAuthClient:
             self.__authenticate()
             headers["Authorization"] = f"Bearer {self.token}"
 
-        response = method(url=url, headers=headers, json=json, data=data, auth=auth)
-        response.raise_for_status()
+        try:
+            response = method(url=url, headers=headers, json=json, data=data, auth=auth)
+            response.raise_for_status()
 
-        return response.json()
+            return response.json()
+
+        except HTTPError as error:
+            if 400 <= error.response.status_code < 500:
+                raise JAuthClientException(
+                    message=error.response.text,
+                    status_code=error.response.status_code,
+                )
+
+            if error.response.status_code >= 500:
+                raise JAuthServerException(
+                    message=error.response.text,
+                    status_code=error.response.status_code,
+                )
+
+        except ConnectionError as error:
+            raise JAuthConnectionException(
+                message=f"Error on connection: {error}",
+            )
+
+        except Timeout as error:
+            raise JAuthTimeoutException(
+                message=f"Timeout on request: {error}",
+            )
+
+        except RequestException as error:
+            raise JAuthRequestException(
+                message=f"Error on request: {error}",
+            )
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,20 +1,20 @@
 import unittest
+
 from j_auth_client.client import JAuthClient
 
 
 class TestJAuthClient(unittest.TestCase):
-    
     def setUp(self):
         self.username = "username"
         self.password = "password"
         self.auth_url = "https://auth.example.com"
-        
+
         self.j_auth_client_instance = JAuthClient(
             username=self.username,
             password=self.password,
             auth_url=self.auth_url,
         )
-    
+
     def test_client(self):
         assert self.j_auth_client_instance.token is None
         assert self.j_auth_client_instance.token_expires_at is None
